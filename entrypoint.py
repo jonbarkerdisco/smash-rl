a19f4c41c924138b22316933740161a603e58781
Jon Barker <jonathan.t.barker@gmail.com>
2019-12-20 09:04:06 -0800

    added path statement to get submodules
    added path statement to get submodules

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,8 @@
-
+import sys
 from smashrl.train import _main as run_training
 from tools.scraper.scrape import _main as download_training_data
-
+sys.path.append('./smashrl/')
+sys.path.append('./tools/')
 
 def _main():
     download_training_data('./data')
